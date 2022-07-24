33636cd72dd75cf5e94c321f1eae9ce1b84d5525
connor <dmitry.jatsyshyn@gmail.com>
2022-07-24 14:36:42 +0200

    Added images changing on button click

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,14 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceimageView1: UIImageView!
     @IBOutlet weak var diceimageView2: UIImageView!
     
+    var namesArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive"]
+    var leftDiceNumber = 0
+    var rightDiceNumber = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceimageView1.image = UIImage(named: "DiceFive")
+        diceimageView1.image = UIImage(named: "DiceOne")
         diceimageView2.image = UIImage(named: "DiceFive")
         
     }
 
-
+    // Button
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        leftDiceNumber = leftDiceNumber + 1
+        diceimageView1.image = UIImage(named: namesArray[leftDiceNumber])
+        
+        rightDiceNumber = rightDiceNumber - 1
+        diceimageView2.image = UIImage(named: namesArray[rightDiceNumber]) 
+    
+    }
+    
 }
 
